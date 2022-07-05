1f8dff691a128caf2f0f6f6c9b50d4a330220a3a
Carlos Simon <casimon@redhat.com>
2022-07-05 15:34:28 -0500

    typo and route

--- deploy-ocp.sh
+++ deploy-ocp.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-./mvnw package
+./mvnw clean package
 rm -rf build/target
-cp -f target build/.
+cp -r target build/.
 oc new-build --strategy docker --binary --name jms-fruits 2>/dev/null
-oc start-build jms-fruits --from-dir build --follow 
+oc start-build jms-fruits --from-dir build --follow
 
 oc new-app -i jms-fruits
    oc set env deployment/jms-fruits QUARKUS_BROKER_URL=tcp://amq-standalone.amq-cluster.svc.cluster.local:61616
+
+oc create route edge --service=jms-fruits
