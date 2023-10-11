aa70aa84e4711397c4339ba2bc03abf66d7b31ec
Carlos Simon <casimon@redhat.com>
2023-10-10 19:01:54 -0600

    doc & route update

--- src/main/java/com/redhat/Routes.java
+++ src/main/java/com/redhat/Routes.java
@@ -7,33 +7,38 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 public class Routes extends RouteBuilder {
   private final List<Fruit> fruits = new CopyOnWriteArrayList<>(Arrays.asList(new Fruit("Apple")));
   private final List<String> jsmFruits = new CopyOnWriteArrayList<>(Arrays.asList());
 
+  private static final String DIRECTGET = "direct:getFruits";
+  private static final String DIRECTPOST = "direct:postFruits";
+
+  @ConfigProperty(name = "fruits.port")
+  String port;
+
   @Override
   public void configure() throws Exception {
 
-    restConfiguration().bindingMode(RestBindingMode.json);
+    restConfiguration().bindingMode(RestBindingMode.json).port(port);
     rest("/fruits")
-        .get().route().setBody(e -> fruits).endRest()
-        .post().type(Fruit.class).route()
-        .to("direct:postFruits")
-        .endRest();
-
-    from("direct:postFruits").process().body(Fruit.class, (Fruit f) -> fruits.add(f));
+        .get("/").to(DIRECTGET)
+        .post().type(Fruit.class).to(DIRECTPOST);
+        
+    from(DIRECTGET).setBody(e -> fruits);
+    from(DIRECTPOST).process().body(Fruit.class, (Fruit f) -> fruits.add(f));
 
     rest("/jmsfruits")
-        .get().route().setBody(e -> jsmFruits).endRest()
-        .post().type(Fruit.class).route()
-        .marshal().json(JsonLibrary.Jackson)
-        .to("activemq:myqueue")
-        .transform(constant("Sent!"))
-        .endRest();
+        .get().to(DIRECTGET)
+        .post().type(Fruit.class).to("direct:postJmsFruits");
+
+    from("direct:postJmsFruits").marshal().json(JsonLibrary.Jackson)
+        .to("activemq:myqueue").transform(constant("Sent!"));
 
     from("activemq:myqueue")
-      .process().body(String.class, (String f) -> jsmFruits.add(f));
+        .process().body(String.class, (String f) -> jsmFruits.add(f));
 
   }
 }
